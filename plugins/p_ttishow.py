c23ddb963031b250d81e0cfeb76defc8d8c1674b
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-07-17 16:49:15 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -60,7 +60,7 @@ async def save_group(bot, message):
                                                  caption=(MALIK_PHH.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
-                                                                           InlineKeyboardButton('♻️ Help ♻️', url=f"https://t.me/{temp.U_NAME}?start=help")
+                                                                           InlineKeyboardButton('© Owner ©', url=f"https://t.me/sahid_malik')
                                                                            ],[
                                                                            InlineKeyboardButton('♻️ GROUP RULES ♻️', callback_data='group_rules')
                                                                          ]]
