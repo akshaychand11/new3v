f63e9d66c146803fd2278b3fd6339ca7c695be32
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-07-20 21:41:12 +0530

    Create extra.py

---
+++ plugins/malik/extra.py
@@ -0,0 +1,483 @@
+#malik
+import re
+import os
+import pyrogram
+import asyncio
+from os import environ
+from info import PHT, ADMINS, AUTH_USERS
+from Script import script
+import time
+from typing import List
+from pyrogram.types import Message, ChatPermissions, InlineKeyboardButton
+from database.users_chats_db import db
+from database.ia_filterdb import Media
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from utils import temp, get_size
+
+@Client.on_message(filters.command("starr") & filters.incoming & ~filters.edited)
+async def star(client, message):
+    if len(message.command):
+        buttons = [[
+            InlineKeyboardButton('❇️ Add Me To Your Groups ❇️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            text=(GHHMT),
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
+        return
+
+@Client.on_message(filters.command('malik') & filters.incoming)
+async def get_ststs(bot, message):
+    malik = await message.reply('Wait..')
+    total_users = await db.total_users_count()
+    await malik.edit(
+               text=(GHHMT.format(total_users)),
+               reply_markup=InlineKeyboardMarkup(
+                                      [[
+                                        InlineKeyboardButton('💢 Request to admin 💢', url="https://t.me/m_admins"),
+                                        InlineKeyboardButton('💢 Request to admin 💢', url="https://t.me/m_admins")
+                                                                         
+                                      ]]
+               ),
+               parse_mode='html'
+)
+# Ban py
+
+@Client.on_message(filters.command("ban"))
+async def ban_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    try:
+        await message.chat.kick_member(
+            user_id=user_id
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"{user_first_name}"
+                " Is forbidden."
+            )
+        else:
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"<a href='tg://user?id={user_id}'>"
+                f"{user_first_name}"
+                "</a>"
+                " Is forbidden."
+            )
+
+@Client.on_message(filters.command("tban"))
+async def temp_ban_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    if not len(message.command) > 1:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    until_date_val = extract_time(message.command[1])
+    if until_date_val is None:
+        await message.reply_text(
+            (
+                "Invalid time type specified. "
+                "Expected m, h, or d, Got it: {}"
+            ).format(
+                message.command[1][-1]
+            )
+        )
+        return
+
+    try:
+        await message.chat.kick_member(
+            user_id=user_id,
+            until_date=until_date_val
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"{user_first_name}"
+                f" banned for {message.command[1]}!"
+            )
+        else:
+            await message.reply_text(
+                "Someone else is dusting off..! "
+                f"<a href='tg://user?id={user_id}'>"
+                "Lavane"
+                "</a>"
+                f" banned for {message.command[1]}!"
+            )
+
+
+#unban py
+
+
+@Client.on_message(filters.command(["unban", "unmute"]))
+async def un_ban_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    try:
+        await message.chat.unban_member(
+            user_id=user_id
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Okay, changed ... now "
+                f"{user_first_name} To "
+                " You can join the group!"
+            )
+        else:
+            await message.reply_text(
+                "Okay, changed ... now "
+                f"<a href='tg://user?id={user_id}'>"
+                f"{user_first_name}"
+                "</a> To "
+                " You can join the group!"
+            )
+# mute py
+
+@Client.on_message(filters.command("mute"))
+async def mute_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    try:
+        await message.chat.restrict_member(
+            user_id=user_id,
+            permissions=ChatPermissions(
+            )
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "👍🏻 "
+                f"{user_first_name}"
+                " Lavender's mouth is shut! 🤐"
+            )
+        else:
+            await message.reply_text(
+                "👍🏻 "
+                f"<a href='tg://user?id={user_id}'>"
+                "Of lavender"
+                "</a>"
+                " The mouth is closed! 🤐"
+            )
+
+
+@Client.on_message(filters.command("tmute"))
+async def temp_mute_user(_, message):
+    is_admin = await admin_check(message)
+    if not is_admin:
+        return
+
+    if not len(message.command) > 1:
+        return
+
+    user_id, user_first_name = extract_user(message)
+
+    until_date_val = extract_time(message.command[1])
+    if until_date_val is None:
+        await message.reply_text(
+            (
+                "Invalid time type specified. "
+                "Expected m, h, or d, Got it: {}"
+            ).format(
+                message.command[1][-1]
+            )
+        )
+        return
+
+    try:
+        await message.chat.restrict_member(
+            user_id=user_id,
+            permissions=ChatPermissions(
+            ),
+            until_date=until_date_val
+        )
+    except Exception as error:
+        await message.reply_text(
+            str(error)
+        )
+    else:
+        if str(user_id).lower().startswith("@"):
+            await message.reply_text(
+                "Be quiet for a while! 😠"
+                f"{user_first_name}"
+                f" muted for {message.command[1]}!"
+            )
+        else:
+            await message.reply_text(
+                "Be quiet for a while! 😠"
+                f"<a href='tg://user?id={user_id}'>"
+                "Of lavender"
+                "</a>"
+                " Mouth "
+                f" muted for {message.command[1]}!"
+            )
+
+# user py
+
+from pyrogram.types import Message
+
+
+def extract_user(message: Message) -> (int, str):
+    """extracts the user from a message"""
+    user_id = None
+    user_first_name = None
+
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        user_first_name = message.reply_to_message.from_user.first_name
+
+    elif len(message.command) > 1:
+        if (
+            len(message.entities) > 1 and
+            message.entities[1].type == "text_mention"
+        ):
+            # 0: is the command used
+            # 1: should be the user specified
+            required_entity = message.entities[1]
+            user_id = required_entity.user.id
+            user_first_name = required_entity.user.first_name
+        else:
+            user_id = message.command[1]
+            # don't want to make a request -_-
+            user_first_name = user_id
+
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            print("പൊട്ടൻ")
+
+    else:
+        user_id = message.from_user.id
+        user_first_name = message.from_user.first_name
+
+    return (user_id, user_first_name)
+
+
+# string_handling py
+
+MATCH_MD = re.compile(r'\*(.*?)\*|'
+                      r'_(.*?)_|'
+                      r'`(.*?)`|'
+                      r'(?<!\\)(\[.*?\])(\(.*?\))|'
+                      r'(?P<esc>[*_`\[])')
+
+# regex to find []() links -> hyperlinks/buttons
+LINK_REGEX = re.compile(r'(?<!\\)\[.+?\]\((.*?)\)')
+BTN_URL_REGEX = re.compile(
+    r"(\[([^\[]+?)\]\(buttonurl:(?:/{0,2})(.+?)(:same)?\))"
+)
+
+
+def button_markdown_parser(msg: Message) -> (str, List):
+    # offset = len(args[2]) - len(raw_text)
+    # set correct offset relative to command + notename
+    markdown_note = None
+    if msg.media:
+        if msg.caption:
+            markdown_note = msg.caption.markdown
+    else:
+        markdown_note = msg.text.markdown
+    note_data = ""
+    buttons = []
+    if markdown_note is None:
+        return note_data, buttons
+    #
+    if markdown_note.startswith(COMMAND_HAND_LER):
+        args = markdown_note.split(None, 2)
+        # use python's maxsplit to separate cmd and args
+        markdown_note = args[2]
+    prev = 0
+    for match in BTN_URL_REGEX.finditer(markdown_note):
+        # Check if btnurl is escaped
+        n_escapes = 0
+        to_check = match.start(1) - 1
+        while to_check > 0 and markdown_note[to_check] == "\\":
+            n_escapes += 1
+            to_check -= 1
+
+        # if even, not escaped -> create button
+        if n_escapes % 2 == 0:
+            # create a thruple with button label, url, and newline status
+            if bool(match.group(4)) and buttons:
+                buttons[-1].append(InlineKeyboardButton(
+                    text=match.group(2),
+                    url=match.group(3)
+                ))
+            else:
+                buttons.append([InlineKeyboardButton(
+                    text=match.group(2),
+                    url=match.group(3)
+                )])
+            note_data += markdown_note[prev:match.start(1)]
+            prev = match.end(1)
+        # if odd, escaped -> move along
+        else:
+            note_data += markdown_note[prev:to_check]
+            prev = match.start(1) - 1
+    else:
+        note_data += markdown_note[prev:]
+
+    return note_data, buttons
+
+
+def extract_time(time_val):
+    if any(time_val.endswith(unit) for unit in ('s', 'm', 'h', 'd')):
+        unit = time_val[-1]
+        time_num = time_val[:-1]  # type: str
+        if not time_num.isdigit():
+            return None
+
+        if unit == 's':
+            bantime = int(time.time() + int(time_num))
+        elif unit == 'm':
+            bantime = int(time.time() + int(time_num) * 60)
+        elif unit == 'h':
+            bantime = int(time.time() + int(time_num) * 60 * 60)
+        elif unit == 'd':
+            bantime = int(time.time() + int(time_num) * 24 * 60 * 60)
+        else:
+            # how even...?
+            return None
+        return bantime
+    else:
+        return None
+
+
+def format_welcome_caption(html_string, chat_member):
+    return html_string.format(
+        dc_id=chat_member.dc_id,
+        first_name=chat_member.first_name,
+        id=chat_member.id,
+        last_name=chat_member.last_name,
+        mention=chat_member.mention,
+        username=chat_member.username
+    )
+
+# admin py
+async def admin_check(message: Message) -> bool:
+    if not message.from_user:
+        return False
+
+    if message.chat.type not in ["supergroup", "channel"]:
+        return False
+
+    if message.from_user.id in [
+        777000,  # Telegram Service Notifications
+        1087968824  # GroupAnonymousBot
+    ]:
+        return True
+
+    client = message._client
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    check_status = await client.get_chat_member(
+        chat_id=chat_id,
+        user_id=user_id
+    )
+    admin_strings = [
+        "creator",
+        "administrator"
+    ]
+    # https://git.colinshark.de/PyroBot/PyroBot/src/branch/master/pyrobot/modules/admin.py#L69
+    if check_status.status not in admin_strings:
+        return False
+    else:
+        return True
+
+# report py
+
+@Client.on_message((filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
+async def report_user(bot, message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        reporter = str(message.from_user.id)
+        mention = message.from_user.mention
+        admins = await bot.get_chat_members(chat_id=chat_id, filter="administrators")
+        success = True
+        report = f"𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})" + "\n"
+        report += f"𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {message.reply_to_message.link}"
+        for admin in admins:
+            try:
+                reported_post = await message.reply_to_message.forward(admin.user.id)
+                await reported_post.reply_text(
+                    text=report,
+                    chat_id=admin.user.id,
+                    disable_web_page_preview=True
+                )
+                success = True
+            except:
+                pass
+        if success:
+            await message.reply_text("𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝖽 𝗍𝗈 𝖠𝖽𝗆𝗂𝗇𝗌!")
+
+
+
+
+
+STTS = """<b>🗂𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+👨‍👩‍👧‍👧 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
+🤿 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
+⏳ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+⌛️ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱</b> """
+
+
+GHHMT = """<b>Thanks For {}.User... 💖 
+
+Thanks For Your Support...
+
+𝖩𝗎𝗌𝗍 𝖠𝖽𝖽 𝖮𝗎𝗋 𝖡𝗈𝗍 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 𝖠𝗌 𝖠𝖽𝗆𝗂𝗇, 𝖨𝗍 𝖶𝗂𝗅𝗅 𝖯𝗋𝗈𝗏𝗂𝖽𝖾 𝖬𝗈𝗏𝗂𝖾𝗌 𝖳𝗁𝖾𝗋𝖾... 😎
+
+
+     ♋️ 𝗙𝗲𝗮𝘁𝘂𝗿𝗲𝘀 ♋️
+
+✪ AutoFilter, Manual Filter
+✪ IMDb HD Posters
+✪ IMDb Real Details
+✪ Two Buttons Mode
+✪ Force Subscribe
+✪ Extra Features: download songs, download you tube video, URL Shortner,  
+
+⚙ More Features Adding Soon</b> 😎"""
+
+
+PPC = environ.get("PPC", "https://telegra.ph/file/3b6afd6c6fcd09606ea9f.jpg")
+TG_MAX_SELECT_LEN = environ.get("TG_MAX_SELECT_LEN", "100")
+
