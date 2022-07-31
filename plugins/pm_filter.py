f7ae273d53c01c1f0702c24c24e436e2235ad815
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-07-31 22:53:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -571,7 +571,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.GROUP_R_TXT,
+            text=(GROUP_Rules),
             disable_web_page_preview=True,
             reply_markup=reply_markup,
             parse_mode='html'
