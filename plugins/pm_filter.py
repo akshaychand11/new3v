70bc3ee57fec28668904fa2aa7770491be694333
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-08-11 02:22:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -472,7 +472,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ], [
             InlineKeyboardButton('Home', callback_data='start'),
             InlineKeyboardButton('Close', callback_data='close_data')
-        ]]        reply_markup = InlineKeyboardMarkup(buttons)
+        ]]  
+        reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.ABOUT_TXT.format(temp.B_NAME),
             reply_markup=reply_markup,
