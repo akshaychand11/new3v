eba81db83fb83dd9f9f88c85dbe589c159041bd8
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-08-25 08:24:37 +0530

    Update extra.py

--- plugins/malik/extra.py
+++ plugins/malik/extra.py
@@ -566,7 +566,7 @@ async def telegraph(client, message):
 
 # sticker py
 
-@Client.on_message(filters.command(["stickerid"]))
+@Client.on_message(filters.command(["stickerid", "sticker", "st"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
        await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
