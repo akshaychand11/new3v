8830c228116733c652e80a9c83d1a3aed933418b
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-09-23 08:41:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -254,7 +254,7 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ', url="https://t.me/+gXuMKXOWm1UyOTdl")
+                    InlineKeyboardButton('♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ', url="https://t.me/+An7-8srMjy5hNGE1")
                     ],[
                     InlineKeyboardButton('💢 DEPLOY YOURS 💢',url="https://youtu.be/v7Vbu3u_VrE") 
                 ]
