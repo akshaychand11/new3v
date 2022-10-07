847e91fc958c17b429c9c935838241a91e386dc0
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-07 09:02:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1031,28 +1031,16 @@ async def auto_filter(client, msg, spoll=False):
         cap = f"<b>🏷  Title: {search}\n📡Group : {message.chat.title}\n🤦Requested By : {message.from_user.mention}</b>"
     if imdb and imdb.get('poster'):
         try:
-            dl =await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(180)
-            await dl.delete()  
-            await message.delete() 
+            await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            dlk = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(180)
-            await dlk.delete()  
-            await message.delete() 
+            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
         except Exception as e:
             logger.exception(e)
-            dkk = await message.reply_photo(photo=PPI, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(180)
-            await dkk.delete()  
-            await message.delete() 
+            await message.reply_photo(photo=PPI, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
     else:
-        dmk = await message.reply_photo(photo=PPI, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(180)
-        await dmk.delete()  
-        await message.delete() 
+        await message.reply_photo(photo=PPI, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
     if spoll:
         await msg.message.delete()
 
