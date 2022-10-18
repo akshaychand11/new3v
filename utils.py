4d579c1c97c12eaadd941eaa9663679cf21c4e22
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-18 09:15:01 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import asyncio
 from pyrogram.types import Message, InlineKeyboardButton
 from pyrogram import enums
 from typing import Union, Union
-import shortzy
+from pyrogram import shortzy
 import re
 import os
 from datetime import datetime
