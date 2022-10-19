bf1677a5cacb7fd25d026f684dc6f7f33e9695f8
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-19 06:25:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -47,7 +47,7 @@ async def req_grp_results(bot, msg):
         search = msg.text
         files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
     if not files: return
-    await msg.reply(f'Dear {msg.from_user.mention}!, {total_results} results are already available for your query!', quote = True), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚶 Back to Group 🚶",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML),
+    await msg.reply(f'Dear {msg.from_user.mention}!, {total_results} results are already available for your query!',  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚶 Back to Group 🚶",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML),
     #text=(MBGH..format(message.from_user.mention, total_results', quote = True))),
     #reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚶 Back to Group 🚶",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)      #f'Dear {msg.from_user.mention}!, {total_results} results are already available for your query!', quote = True)
     
