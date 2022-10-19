b5c8fe6380309c5a78c3a7fef2677b438e2d7e19
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-19 06:04:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -47,8 +47,8 @@ async def req_grp_results(bot, msg):
         search = msg.text
         files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
     if not files: return
-    await msg.reply(
-           text=(MBGH..format(message.from_user.mention, total_results))
+    await msg.reply_text(
+           text=(MBGH..format(message.from_user.mention, total_results)),
            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚶 Back to Group 🚶",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)       #f'Dear {msg.from_user.mention}!, {total_results} results are already available for your query!', quote = True)
     
 
