85cdb8796c3417c4549b1bd365841121648945ef
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-20 23:46:17 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -80,7 +80,7 @@ async def get_search_results(query, file_type=None, max_results=temp.multi_butto
     elif ' ' not in query:
         raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
-        raw_pattern = query.replace('', r'.*[@\s\.\+\-_]')
+        raw_pattern = query.replace('', r'.*[\@\s\.\+\-_]')
     
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
@@ -125,7 +125,7 @@ async def get_filter_results(query):
     elif ' ' not in query:
         raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
-        raw_pattern = query.replace('', r'.*[\s\.\+\-_]')
+        raw_pattern = query.replace('', r'.*[\@\s\.\+\-_]')
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
     except:
