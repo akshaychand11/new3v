8b9715a5b3fe740342c6202d8292465a8432bf4d
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-24 22:17:28 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,4 +1,4 @@
-from info import BOT_USERNAME, DEL_SECOND
+from info import BOT_USERNAME, DEL_SECOND, DELL_SECOND
 from plugins.malik.extra import IYGL, YTILK, PPI, SMART_PIC
 
 
@@ -10,10 +10,12 @@ class malik(object):
       int_link = IYGL
       smart_pic = SMART_PIC 
       delete = DEL_SECOND
+      dell_second = DELL_SECOND
     #  mntf = MNTFN
      # ntatm =  PPI
       ytilk = YTILK
      # mqtt = MQTT
      # tfarde = TFRADE
+      
       
 
