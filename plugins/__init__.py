33dd57743cefcbb63cb1b468ca00f77f6d17cb86
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-28 00:06:17 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,5 +1,5 @@
 from info import BOT_USERNAME, DEL_SECOND, DELL_SECOND
-from plugins.malik.extra import IYGL, YTILK, PPI, SMART_PIC
+from plugins.malik.extra import IYGL, J_GROUP, J_GROUP2, YTILK, PPI, SMART_PIC
 
 
 
@@ -11,6 +11,8 @@ class malik(object):
       smart_pic = SMART_PIC 
       delete = DEL_SECOND
       dell_second = DELL_SECOND
+      j_group = J_GROUP
+      j_group2 = J_GROUP2
       notfoun = IYGL
      # mntf = MNTFN
       #ntatm =  PPI
