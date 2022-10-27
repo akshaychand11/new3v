d9a38a4ad6ce7478fac2a615154cd32aea6db393
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-27 23:13:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -414,12 +414,23 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                 return
             else:
-                await client.send_cached_media(
+                dl = await client.send_cached_media(
                     chat_id=query.from_user.id,
                     file_id=file_id,
                     caption=f_caption,
                     protect_content=True if ident == "filep" else False 
+                    reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                          InlineKeyboardButton('♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ', url="https://t.me/+FAgX05kGByNkZjJl")
+                          ],[
+                          InlineKeyboardButton('💢 DEPLOY YOURS 💢',url="https://youtu.be/v7Vbu3u_VrE") 
+                        ]
+                    ]
                 )
+            )
+            await asyncio.sleep(298)
+            await dl.delete()
                 await query.answer('Check PM, I have sent files in pm', show_alert=True)
                 # print(query.message.chat.type)
                 #if not query.message.chat.type  == "private":await query.answer('Check PM, I have sent files in pm', show_alert=True)
