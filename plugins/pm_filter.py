647390ea5d1d0eb8885fd06d4349e19426126cf0
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-30 14:47:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -32,7 +32,7 @@ BUTTONS = {}
 SPELL_CHECK = {}
 
 
-@Client.on_message(filters.text & filters.incoming)
+@Client.on_message(filters.text & filters.private & filters.incoming)
 async def give_filter(client, message):
     if MAINTENANCE_MODE:
         if AUTH_USERS and message.from_user and message.from_user.id in AUTH_USERS:
