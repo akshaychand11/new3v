c0222cc9b7c80315c2d39de285f625600c971190
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-30 13:37:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -47,7 +47,7 @@ async def give_filter(client, message):
       #  ]
        # ]
         #await message.reply_text(f"🔰𝗡𝗢𝗧𝗜𝗖𝗘🔰\n\nService is 𝕔𝕝𝕠𝕤𝕖𝕕 for 𝟮 𝘄𝗲𝗲𝗸𝘀.\nwill start again by <u>next month.</u>.\n\n𝖡𝗒 𝗍𝗁𝗂𝗌 𝗍𝗂𝗆𝖾, 𝖬𝖺𝗄𝖾 𝗌𝗎𝗋𝖾 <b>you have 𝗌𝗎𝖻𝗌𝖼𝗋𝗂𝖻𝖾𝖽 CINEMA HUB group👇🏻</b>", reply_markup=InlineKeyboardMarkup(btn))    
-    else:
+   # else:
         k = await manual_filters(client, message)
         if k == False:
             await auto_filter(client, message)
