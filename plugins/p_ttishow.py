6cc76f8a60e512c57e659c8ba88fdfd61c51c96f
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-30 20:29:19 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -312,13 +312,3 @@ async def give_filter(client, message):
 
 
 
-@Client.on_message(filters.text & filters.incoming)
-async def give_filter(client, message):
-    if MAINTENANCE_MODE:
-        if AUTH_USERS and message.from_user and message.from_user.id in AUTH_USERS:
-            k = await manual_filters(client, message)
-            if k == False:
-                await auto_filter(client, message)
-        else:
-            await message.reply_text(text=(PM_FILTER.format(message.from_user.mention)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚶 Back to Group 🚶",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)#"You are now verified for next 24 hours. Continue asking movies")      
-
