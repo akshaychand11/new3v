0ff9a5d8a2cb601d10d0f408da3b2290b6b2dc2b
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-31 13:30:47 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -300,15 +300,4 @@ async def list_chats(bot, message):
 
 
 
-@Client.on_message(filters.text & filters.private & filters.incoming)
-async def give_filter(client, message):
-    if PM_MAINTENANCE_MODE:
-        if AUTH_USERS and message.from_user and message.from_user.id in AUTH_USERS:
-            k = await manual_filters(client, message)
-            if k == False:
-                await auto_filter(client, message)
-        else:
-            await message.reply_text(text=(G_FILTER.format(message.from_user.mention)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡️ Backup Channel ⚡️",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)#"You are now verified for next 24 hours. Continue asking movies")      
-
-
 
