4121760b5adb72a8527d1b35556bacc9383f9ebd
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-10-31 13:35:42 +0530

    Update t_pm.py

--- plugins/t_pm.py
+++ plugins/t_pm.py
@@ -1,5 +1,20 @@
 #sahid malik
-from info import MAINTENANCE_MODE, AUTH_USERS
+from plugins.malik.extra import G_FILTER
+from info import PM_MAINTENANCE_MODE, AUTH_USERS
 from pyrogram import Client, filters, enums 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+
+@Client.on_message(filters.text & filters.private & filters.incoming)
+async def give_filter(client, message):
+    if PM_MAINTENANCE_MODE:
+        if AUTH_USERS and message.from_user and message.from_user.id in AUTH_USERS:
+            k = await manual_filters(client, message)
+            if k == False:
+                await auto_filter(client, message)
+        else:
+            await message.reply_text(text=(G_FILTER.format(message.from_user.mention)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡️ Backup Channel ⚡️",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)#"You are now verified for next 24 hours. Continue asking movies")      
+
 
