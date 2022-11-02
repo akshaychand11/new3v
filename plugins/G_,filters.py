62ced50ac29df0457c18d8a87a4be53359af8085
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-02 08:02:37 +0530

    Update G_,filters.py

--- plugins/G_,filters.py
+++ plugins/G_,filters.py
@@ -52,7 +52,7 @@ async def filter(client, message):
             return
     if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
         return
-    if 2 < len(message.text) < 100:    
+    if 2 < len(message.text) < 50:    
         btn = []
         search = message.text
         mo_tech_yt = f"**🗂️ Title:** {search}\n**"
