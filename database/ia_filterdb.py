2f7f821762320c0aa7d3f68d8988aaca035c124b
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-02 23:05:24 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -77,10 +77,10 @@ async def get_search_results(query, file_type=None, max_results=temp.multi_butto
         #raw_pattern = r'(\s|_|\-|\.|\+)' + query + r'(\s|_|\-|\.|\+)'
     if not query:
         raw_pattern = '-'
-    elif '@' not in query:
-        raw_pattern = r'(\b|[\.\+\-_\+\-@])' + query + r'(\b|[\.\+\-_\+\-@])'
+    elif ' ' not in query:
+        raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
-        raw_pattern = query.replace('@','', r'.*[\s\.\+\-_\+\-@]')
+        raw_pattern = query.replace('@','', r'.*[\s\.\+\-_]\-@')
     
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
