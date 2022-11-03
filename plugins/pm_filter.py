83acb61b4ca59188996b09bb6269990a09f7b8a3
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-03 22:32:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -424,7 +424,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     caption=f_caption,
                     protect_content=True if ident == "filep" else False,
                     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ', url=malik.grp_it_lk)],[InlineKeyboardButton("⚙ DEPLOY YOURS", url=malik.dp_yrs),]]),parse_mode=enums.ParseMode.HTML)
-                    return 
+                return 
                 #await query.answer('Check PM, I have sent files in pm', show_alert=True)
                 # print(query.message.chat.type)
                 #if not query.message.chat.type  == "private":await query.answer('Check PM, I have sent files in pm', show_alert=True)
