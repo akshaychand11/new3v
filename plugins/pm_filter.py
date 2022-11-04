1a427600ee6e017c6f923e9c0cde00bfb5756489
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-04 12:44:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -49,7 +49,7 @@ async def filter(client, message):
                 file_id = file.file_id
                 filename = f"[{get_size(file.file_size)}] {file.file_name}"
                 btn.append(
-                    [InlineKeyboardButton(text=f"{file.file_name}",callback_data=f"files#{file_id}")]
+                    [InlineKeyboardButton(text=f"{filename}",callback_data=f"files#{file_id}")]
                     )
         else:
             await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
