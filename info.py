04e02a696dcf522f80745c258bf0e9d1892f3f34
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-04 12:21:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,3 +121,20 @@ PM_MAINTENANCE_MODE = is_enabled((environ.get('PM_MAINTENANCE_MODE', "False")), 
 SHORT_URLL = is_enabled((environ.get('SHORT_URL', "False")), False)
 SHORTENER_API2 = environ.get("SHORTENER_API", None)
 LONG_MEGHA_URL = environ.get("LONG_MEGHA_URL", False)
+
+
+
+#test 
+
+FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
+OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
+if FILE_CAPTION.strip() == "":
+    CUSTOM_FILE_CAPTION=None
+else:
+    CUSTOM_FILE_CAPTION=FILE_CAPTION
+if OMDB_API_KEY.strip() == "":
+    API_KEY=None
+else:
+    API_KEY=OMDB_API_KEY
+
+
