ec34244f2cd51f72969fa2eb800751d2fd9909db
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-05 23:01:57 +0530

    Update pr_filter.py

--- plugins/pr_filter.py
+++ plugins/pr_filter.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pyrogram import Client, filters
 import re
 from pyrogram.errors import UserNotParticipant
-from database.ia_filterdb import get_search_results, get_file_details
+from database.ia_filterdb import get_filter_results, get_search_results, get_file_details
 from utils import get_size, is_subscribed, get_poster
 import random
 BUTTONS = {}
