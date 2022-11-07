dab266992cbe8759c42e3e4065272ba0d7e13e0f
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-07 23:30:57 +0530

    Update pr_filter.py

--- plugins/pr_filter.py
+++ plugins/pr_filter.py
@@ -1033,7 +1033,7 @@ async def auto_filter(client, msg, spoll=False):
             return
         if 2 < len(message.text) < 50:
             search = message.text.replace(' - ', '').replace('movie', '').replace('[', '').replace(']', '').replace('gujarati', '').replace('gujrati', '').replace('punjabi', '').replace('marathi', '').replace('movies', '').replace(':', '').replace(',', '').replace('(', '').replace(')', '').replace('@FilmeHDPlay', '')
-            files, offsett, total_resultss = await get_search_results(search.lower(), offsett=0, filter=True)
+            files, offsett, total_resultss = await get_searchh_results(search.lower(), offsett=0, filter=True)
             if not files:
                 if settings["spell_check"]:
                     return await advantage_spell_chok(msg, message)
