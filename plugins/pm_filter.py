6b258f3575539ea17697da7c76af8672cef15cce
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-12 14:17:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -16,7 +16,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pyrogram import Client, filters, enums 
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
-from plugins.shortzy_filte import get_shortlink
+from plugins.shortzy_filter import get_shortlink
 from database.users_chats_db import db
 from database.ia_filterdb import Media, get_file_details, get_search_results, get_filter_results
 from database.filters_mdb import (
