2483df03e5f0c0d8d3979fcfd3401e7877755725
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-12 14:52:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1037,7 +1037,7 @@ async def auto_filter(client, msg, spoll=False):
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
                 if settings["spell_check"]:
-                    return await advantage_spell_chok(msg, message)
+                    return await advantage_spell_chok(client, msg, message)
                 else:
                     return
         else:
