7c5b63e33397ef5ff52489b5c322ea80bcbab12e
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-13 23:12:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -167,7 +167,7 @@ Time : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
 
 #New_Verifie_User"""
         await client.send_message(LOG_CHANNEL2, text)
-        await m.reply_photo(photo=(MALIK), f"he how are you", #coption=(MALIK2.format(message.from_user.mention)),
+        await m.reply_photo(photo="https://telegra.ph/file/650c8b857763f8d96892c.jpg", caption="he how are you", #coption=(MALIK2.format(message.from_user.mention)),
            # photo=(MALIK), #caption=(MALIK2)),
             #caption=(MALIK2.format(message.from_user.mention)), 
            # protect_content = True if user_id == 'm.from_user_id' else True,
