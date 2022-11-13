10bda223a091f593aac8ffb6ef6116240b4d32dc
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-13 14:18:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -171,7 +171,6 @@ Time : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
         dmb = await m.reply_photo(
             photo=(MALIK), #caption=(MALIK2)),
             caption=(MALIK2.format(message.from_user.mention)),
-            protect_content = True if verify == 'm' else True,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
