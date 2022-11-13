c510205c013232ea2f4416c61b76eb06d1f8c0c4
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-13 08:41:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -169,8 +169,8 @@ Time : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
         dmb = await m.reply_photo(
             photo=(MALIK), #caption=(MALIK2)),
             caption=(MALIK2.format(message.from_user.mention)),
-            protect_content=msg.get('protect', True),
             reply_markup=reply_markup,
+            protect_content=msg.get('protect', True),
             parse_mode=enums.ParseMode.HTML
         )
         await asyncio.sleep(120) 
