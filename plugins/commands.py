b434f4722422408b9fd3d1f07bfd8c62a722faa3
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-13 23:58:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -110,7 +110,6 @@ Time : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
                 btn.append([InlineKeyboardButton("♻️ Try Again ♻️", callback_data=f"{pre}#{file_id}")])
             except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
             text=script.FORCESUB_TXT,
