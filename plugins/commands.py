a246add8d3197d75753806cd1b29843e10e921da
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-13 09:09:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 BATCH_FILES = {}
 
 @Client.on_message(filters.command("start") & filters.incoming)
-async def start(client:Client, message, msg):
+async def start(client:Client, message):
     
     m = message
     user_id = m.from_user.id
@@ -169,6 +169,7 @@ Time : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
         dmb = await m.reply_photo(
             photo=(MALIK), #caption=(MALIK2)),
             caption=(MALIK2.format(message.from_user.mention)),
+            protect_content=True,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
