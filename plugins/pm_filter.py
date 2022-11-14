5667efdb0532ca8f6b00b6f0ef01e5a3e8cb2771
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-14 14:29:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1088,24 +1088,24 @@ async def auto_filter(client, msg, spoll=False):
    
     cap = f"<b>🏷  Title: {search}\n📡Group : {message.chat.title}\n🤦Requested By : {message.from_user.mention}</b>",
        # try:
-          ab = await message.reply_photo(photo=malik.smart_pic, caption=cap,  reply_markup=InlineKeyboardMarkup(btn))
-          await asyncio.sleep(malik.delete)
-          await ab.delete()
-          await message.delete()
+        ab = await message.reply_photo(photo=malik.smart_pic, caption=cap,  reply_markup=InlineKeyboardMarkup(btn))
+        await asyncio.sleep(malik.delete)
+        await ab.delete()
+        await message.delete()
       #  except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):  
-            pic = imdb.get('poster')
-            poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await message.reply_photo(photo=malik.smart_pic, caption=cap,  reply_markup=InlineKeyboardMarkup(btn))
-            abb = await asyncio.sleep(malik.delete)
-            await abb.delete()
-            await message.delete()
+            #pic = imdb.get('poster')
+            #poster = pic.replace('.jpg', "._V1_UX360.jpg")
+        await message.reply_photo(photo=malik.smart_pic, caption=cap,  reply_markup=InlineKeyboardMarkup(btn))
+        abb = await asyncio.sleep(malik.delete)
+        await abb.delete()
+        await message.delete()
        # except Exception as e:
           #  logger.exception(e)
-            abbb = await message.reply_photo(photo=malik.smart_pic, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(malik.delete)
-            await abbb.delete()
-            await message.delete()
-    else:
+        abbb = await message.reply_photo(photo=malik.smart_pic, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+        await asyncio.sleep(malik.delete)
+        await abbb.delete()
+        await message.delete()
+   # else:
         abbb = await message.reply_photo(photo=malik.smart_pic, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
         await asyncio.sleep(malik.delete)
         await abbb.delete()
