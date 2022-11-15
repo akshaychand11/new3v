a04efe47468719e7ddc743f2681b4f2cfa7696ea
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-15 04:34:48 +0000

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1124,12 +1124,12 @@ async def auto_filter(client, msg, spoll=False):
         cap = f"<b>🏷  Title: {search}\n📡Group : {message.chat.title}\n🤦Requested By : {message.from_user.mention}</b>"
     if imdb and imdb.get('poster'):
         try:
-            await message.reply_photo(photo=imdb.get('poster'), caption=cap[:"_V1_UX360.jpg"], reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_photo(photo=malik.smart_pic, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
             protect_content = True 
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):  
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await message.reply_photo(photo=imdb.get('poster'), caption=cap[:_V1_UX360.jpg"], reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_photo(photo=malik.smart_pic, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
             protect_content = True 
         except Exception as e:
             logger.exception(e)
