1ada898c0a9ce646284f0d2552d032efdee393bf
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-17 22:30:20 +0530

    Update t_pm.py

--- plugins/t_pm.py
+++ plugins/t_pm.py
@@ -8,12 +8,11 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 @Client.on_message(filters.text & filters.private & filters.incoming)
-async def give_filter(client, message):
+async def give_text(client, message):
     if PM_MAINTENANCE_MODE:
         content = message.text
-        user = message.from_user.first_name
-        user_id = message.from_user.id
+        user = message.from_user.mention
         if content.startswith("/") or content.startswith("#"): return #
-        await message.reply_text(text=(G_FILTER.format(message.from_user.mention)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡️ Backup Channel ⚡️",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)#"You are now verified for next 24 hours. Continue asking movies")      
+        await message.reply_text(text=(G_FILTER.format(user)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡️ Backup Channel ⚡️",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)#"You are now verified for next 24 hours. Continue asking movies")      
 
 
