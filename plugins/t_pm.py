7ee145350e779805ccf95c1eafaf90bc9ca302e9
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-17 22:41:58 +0530

    Update t_pm.py

--- plugins/t_pm.py
+++ plugins/t_pm.py
@@ -12,7 +12,17 @@ async def give_text(client, message):
     if PM_MAINTENANCE_MODE:
         content = message.text
         user = message.from_user.mention
-        if content.startswith("/") or content.startswith("#"): return #
+        if content.startswith("/") return #
         await message.reply_text(text=(G_FILTER.format(user)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡️ Backup Channel ⚡️",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)#"You are now verified for next 24 hours. Continue asking movies")      
 
-
+@Client.on_message(filters.text & filters.private & filters.incoming)
+async def g_text(client, message):
+    if PM_MAINTENANCE_MODE:
+        content = message.text
+        user = message.from_user.first_name
+        if content.startswith("/") or content.startswith("#"): return #
+        await message.reply_text("<b>Your message has been sent to my moderators !</b>")
+        await bot.send_message(
+            chat_id=LOG_CHANNEL,
+            text=f"<b>#PM_MSG\n\nName : {user}\n\nID : {user_id}\n\nMessage : {content}</b>"
+        )
