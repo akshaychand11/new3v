60354483075440e5195fa0f5e12503d6dd20256c
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-17 22:26:16 +0530

    Update t_pm.py

--- plugins/t_pm.py
+++ plugins/t_pm.py
@@ -10,11 +10,10 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(filters.text & filters.private & filters.incoming)
 async def give_filter(client, message):
     if PM_MAINTENANCE_MODE:
-        if AUTH_USERS and message.from_user and message.from_user.id in AUTH_USERS:
-            k = await manual_filters(client, message)
-            if k == False:
-                await auto_filter(client, message)
-        else:
-            await message.reply_text(text=(G_FILTER.format(message.from_user.mention)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡️ Backup Channel ⚡️",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)#"You are now verified for next 24 hours. Continue asking movies")      
+        content = message.text
+        user = message.from_user.first_name
+        user_id = message.from_user.id
+        if content.startswith("/") or content.startswith("#"): return #
+        await message.reply_text(text=(G_FILTER.format(message.from_user.mention)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡️ Backup Channel ⚡️",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)#"You are now verified for next 24 hours. Continue asking movies")      
 
 
