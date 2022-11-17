be80bb2da3732309909a72a72e9806aeeb9358a0
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-17 23:18:50 +0530

    Update t_pm.py

--- plugins/t_pm.py
+++ plugins/t_pm.py
@@ -4,6 +4,32 @@ from info import PM_MAINTENANCE_MODE, LOG_CHANNEL
 from pyrogram import Client, filters, enums 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
+@Client.on_message(filters.command("send") & filters.user(ADMINS))
+async def send_msg(bot, message):
+    if message.reply_to_message:
+        target_id = message.text
+        command = ["/send "]
+        for cmd in command:
+            if cmd in target_id:
+                target_id = target_id.replace(cmd, "")
+        success = False
+        try:
+            user = await bot.get_users(int(target_id))
+            await message.reply_to_message.copy(int(user.id))
+            success = True
+        except Exception as e:
+            await message.reply_text(f"<b>Must give a valid chat id ! \nError: {e}</b>")
+        if success:
+            await message.reply_text(f"<b>Your message has been successfully send to {user.mention}.</b>")
+        else:
+            await message.reply_text("<b>An error occurred !</b>")
+    else:
+        await message.reply_text("<b>Use this command as a reply to any message using the target chat id. For eg: /send userid</b>")
+
+
+
+
 @Client.on_message(filters.text & filters.private & filters.incoming)
 async def g_text(bot, message):
         content = message.text
