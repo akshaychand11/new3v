3789c231147d3d6d11974133d4636e7896d49632
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-17 22:55:30 +0530

    Update t_pm.py

--- plugins/t_pm.py
+++ plugins/t_pm.py
@@ -13,18 +13,7 @@ async def give_text(client, message):
         user = message.from_user.mention
         if content.startswith("/") return #
         await message.reply_text(text=(G_FILTER.format(user)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡️ Backup Channel ⚡️",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)  
-        return 
+        
 
 
 
-
-@Client.on_message(filters.text & filters.private & filters.incoming)
-async def give_text(client, message):
-        contents = msg.text
-        user = msg.from_user.first_name
-        if content.startswith("/") or contents.startswith("#"): return #
-        await msg.reply_text("<b>Your message has been sent to my moderators !</b>")
-        await bot.send_msg(
-            chat_id=LOG_CHANNEL,
-            text=f"<b>#PM_MSG\n\nName : {user}\n\nID : {user_id}\n\nMessage : {contents}</b>"
-        )
