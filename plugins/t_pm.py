89a8fead4feeb92f8f1a2dd485ffe3224225fa88
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-17 22:54:16 +0530

    Update t_pm.py

--- plugins/t_pm.py
+++ plugins/t_pm.py
@@ -4,7 +4,6 @@ from info import PM_MAINTENANCE_MODE, PM_MAINTENANCE_MODE2, LOG_CHANNEL
 from pyrogram import Client, filters, enums 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-malik = Client
 
 
 @Client.on_message(filters.text & filters.private & filters.incoming)
@@ -15,13 +14,3 @@ async def give_text(client, message):
         if content.startswith("/") return #
         await message.reply_text(text=(G_FILTER.format(user)), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡️ Backup Channel ⚡️",url="https://t.me/+FAgX05kGByNkZjJl"),]]),parse_mode=enums.ParseMode.HTML)  
 
-@malik.on_message(filters.text & filters.private & filters.incoming)
-async def give_text(client, message):
-        contents = msg.text
-        user = msg.from_user.first_name
-        if content.startswith("/") or contents.startswith("#"): return #
-        await msg.reply_text("<b>Your message has been sent to my moderators !</b>")
-        await bot.send_msg(
-            chat_id=LOG_CHANNEL,
-            text=f"<b>#PM_MSG\n\nName : {user}\n\nID : {user_id}\n\nMessage : {contents}</b>"
-        )
