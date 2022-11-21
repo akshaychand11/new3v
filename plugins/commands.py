dcf148ca6b26ba6b58cb23b96cab20dcb2cc07cc
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-21 23:45:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -309,6 +309,7 @@ Time : {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
         file_id=file_id,
         caption=f_caption,
         protect_content=True if pre == 'filep' else False,
+        await client.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, mv_rqst)))
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('♻️ 𝐉𝐨𝐢𝐧 𝙂𝙧𝙤𝙪𝙥 ', url=malik.grp_it_lk)],[InlineKeyboardButton("⚙ DEPLOY YOURS", url=malik.dp_yrs),]]),parse_mode=enums.ParseMode.HTML)   
     await asyncio.sleep(298)
     await dl.delete()
