23a4175f705e779487c941ea568ce68e4113e99d
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-22 08:15:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1277,13 +1277,8 @@ async def auto_filter(client, msg, spoll=False):
     if spoll:
         await msg.message.delete()
 
-async def advantage_spell_chok(client, msg, message):
-    mv_rqst = msg.text
-    reqstr1 = msg.from_user.id if msg.from_user else 0
-    reqstr = await client.get_users(reqstr1)
-    settings = await get_settings(message.chat.id)
-    user = msg.from_user.id if msg.from_user else 0
- query = re.sub(
+async def advantage_spell_chok(msg):
+    query = re.sub(
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
     malik = query.strip() + " movie"
@@ -1325,6 +1320,7 @@ async def advantage_spell_chok(client, msg, message):
         gs_parsed = gs_parsed[:3]
     if gs_parsed:
         for mov in gs_parsed:
+            imdb_s = await get_poster(mov.strip(), bulk=True)  # searching each keyword in imdb
             if imdb_s:
                 movielist += [movie.get('title') for movie in imdb_s]
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
@@ -1345,7 +1341,7 @@ async def advantage_spell_chok(client, msg, message):
         await asyncio.sleep(100) 
         await a.delete()
         return
-    SPELL_CHECK[msg.id] = g_s
+    SPELL_CHECK[msg.id] = movielist
     btn = [[
         InlineKeyboardButton(
             text=movie.strip(),
